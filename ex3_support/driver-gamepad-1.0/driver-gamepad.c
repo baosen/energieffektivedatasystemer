1392056004c1e146f514ab9f1a4467491ae2a7ad
bao <bcnguyen@stud.ntnu.no>
2014-04-24 12:39:04 +0200

    Added mounting to \/dev\/gamepad on startup.

--- ex3_support/driver-gamepad-1.0/driver-gamepad.c
+++ ex3_support/driver-gamepad-1.0/driver-gamepad.c
@@ -2,6 +2,7 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/device.h>
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
@@ -13,19 +14,15 @@ MODULE_AUTHOR("asdf");
 MODULE_DESCRIPTION("Gamepad driver.");
 MODULE_LICENSE("Dual BSD/GPL");
 
-// user program reads from the driver.
-static ssize_t gamepad_read(struct file *, char *, size_t, loff_t *);
-// user program writes to the driver.
-static ssize_t gamepad_write(struct file *, const char *, size_t, loff_t *);
-// user program opens the driver.
-static int gamepad_open(struct inode *, struct file *);
-// user program closes the driver.
-static int gamepad_release(struct inode *, struct file *);
+static ssize_t gamepad_read(struct file *, char *, size_t, loff_t *);        // user program reads from the driver.
+static ssize_t gamepad_write(struct file *, const char *, size_t, loff_t *); // user program writes to the driver.
+static int gamepad_open(struct inode *, struct file *);                      // user program opens the driver.
+static int gamepad_release(struct inode *, struct file *);                   // user program closes the driver.
 
 static struct file_operations fops = {
-    .read = gamepad_read,
-    .write = gamepad_write,
-    .open = gamepad_open,
+    .read    = gamepad_read,
+    .write   = gamepad_write,
+    .open    = gamepad_open,
     .release = gamepad_release
 };
 
@@ -33,8 +30,7 @@ static struct file_operations fops = {
 
 static struct cdev character_device;
 static dev_t device_number;
-
-static int device_is_open  = 0; // is device opened by another file?
+static int device_is_open = 0; // is device opened by another file?
 
 static void enable_joystick(void)
 {
@@ -56,11 +52,11 @@ static void enable_gpio_clock()
 }
 
 // Enable GPIO output to output current to the LEDs.
-static void enable_gpio_led_output()
-{
-    *GPIO_PA_CTRL = 2;                         /* set high drive strength */
-    *GPIO_PA_MODEH = 0x55555555;               /* set pins A8-15 as output */
-}
+//static void enable_gpio_led_output()
+//{
+//    *GPIO_PA_CTRL  = 2;          // set high drive strength.
+//    *GPIO_PA_MODEH = 0x55555555; // set pins A8-15 as output.
+//}
 
 // Function to set up GPIO mode and interrupts.
 static void setup_gpio()
@@ -70,43 +66,42 @@ static void setup_gpio()
     enable_joystick();
 }
 
+static struct class* device_class;
+
 // gamepad_init - function to insert this module into kernel space.
 // 
 // This is the first of two exported functions to handle inserting this
 // code into a running kernel
 // 
 // Returns 0 if successful, otherwise -1
-
 static int __init gamepad_init(void)
 {
 	setup_gpio();
 
-    int ret = alloc_chrdev_region(&device_number, 0, 1, DEVICE_NAME);
-    if (ret < 0) {
+    int return_value = alloc_chrdev_region(&device_number, 0, 1, DEVICE_NAME);
+
+    if (return_value < 0) {
         printk(KERN_ALERT "gamepad: failed to allocate major number.\n");
-        return ret;
+        return return_value;
     }
-    else {
-        printk(KERN_INFO "major number allocated successful");
-    }
+    printk(KERN_INFO "gamepad: major number allocated successful\n");
 
-    int major_number = MAJOR(device_number); // get the major number to this device.
-    printk(KERN_INFO "gamepad: major number of our device is %d\n", major_number);
-    printk(KERN_INFO "gamepad: to use mknod /dev/%s c %d 0\n", DEVICE_NAME, major_number);
+    // OLD:
+    // int major_number = MAJOR(device_number); // get the major number to this device.
+    // printk(KERN_INFO "gamepad: major number of our device is %d\n", major_number);
+    // printk(KERN_INFO "gamepad: to use mknod /dev/%s c %d 0\n", DEVICE_NAME, major_number);
 
-    cdev_init(&character_device, &fops); // The new way used in the compendium.
+    cdev_init(&character_device, &fops); // The new way to initialize a character device driver, according to the compendium.
     int error_code = cdev_add(&character_device, device_number, 1);
+
     if (error_code < 0) {
-        printk(KERN_ALERT "Registering character device failed with %d\n", error_code);
+        printk(KERN_ALERT "gamepad: registering character device failed with %d\n", error_code);
         return error_code;
     }
 
-    // device_number = alloc_chrdev_region();
-
-    // device_class = class_create(THIS_MODULE, "gamepad");
-    // device_create = (device_class, NULL, device_number, NULL, "gamepad");
-
-    printk(KERN_ALERT "Hello, world from gamepad driver!\n");
+    // Mounts the gamepad driver as /dev/gamepad.
+    device_class = class_create(THIS_MODULE, DEVICE_NAME);
+    device_create(device_class, NULL, device_number, NULL, DEVICE_NAME);
 
 	return 0;
 }
@@ -125,8 +120,7 @@ static void __exit gamepad_exit(void)
 }
 
 
-// Called when a process tries to open the device file, like
-// "cat /dev/mycharfile"
+// Called when a process tries to open the device file, e.g cat /dev/mycharfile.
 static int gamepad_open(struct inode *inode, struct file *file)
 {
     if (device_is_open) {
@@ -154,19 +148,20 @@ static ssize_t gamepad_read(struct file *filp, char *buffer, size_t count, loff_
     // buf[2] = '\n';
     // buf[3] = '\0';
     
-    if (*offset >= 4)
+    if (*offset >= 4) {
         return END_OF_FILE;
+    }
 
     copy_to_user(buffer, *GPIO_PC_DIN, 4);
     *offset += 4;
 
-	return 4;
+	return 4; // returns the number of bytes read.
 }
  
-// Called when a process writes to dev file: echo "hi" > /dev/hello 
+// Called when a process writes to dev file e.g echo "hi" > /dev/hello 
 static ssize_t gamepad_write(struct file *filp, const char *buff, size_t count, loff_t * off)
 {
-	printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
+	printk(KERN_ALERT "Sorry, this operation isn't supported yet...\n");
 	return -EINVAL;
 }
 
